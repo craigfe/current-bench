6ea83660ae23dc7d4371dd550152af91223e7252
Craig Ferguson <me@craigfe.io>
2020-01-06 13:28:32 +0100

    Add skeleton for the Irmin backend

---
+++ backend/README.md
@@ -0,0 +1,10 @@
+# irmin_datastore
+
+Irmin database for benchmarking data exposed via a GraphQL interface
+
+## Installation
+
+```
+opam pin add --yes https://github.com/CraigFe/irmin_datastore.git
+opam install irmin_datastore
+```
---
+++ backend/bin/main.ml
@@ -0,0 +1,19 @@
+let main () = Irmin_datastore.main () |> print_endline
+
+open Cmdliner
+
+let setup_log =
+  let init style_renderer level =
+    Fmt_tty.setup_std_outputs ?style_renderer ();
+    Logs.set_level level;
+    Logs.set_reporter (Logs_fmt.reporter ())
+  in
+  Term.(const init $ Fmt_cli.style_renderer () $ Logs_cli.level ())
+
+let term =
+  let doc = "Irmin database for benchmarking data exposed via a GraphQL interface" in
+  let exits = Term.default_exits in
+  let man = [] in
+  Term.(const main $ setup_log, info "irmin_datastore" ~doc ~exits ~man)
+
+let () = Term.exit (Term.eval term)
